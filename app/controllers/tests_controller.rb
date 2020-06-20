327210008b449753f9081f4601ce1d5f05e71460
pharia-le <pharia.le@gmail.com>
2020-06-20 15:54:22 -0700

    Remove instance variables & uncomment actions.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,50 +1,43 @@
 class TestsController < ApplicationController
   before_action :set_test, only: [:show, :update, :destroy]
 
-  # GET /tests
   def index
-    @tests = Test.all
+    tests = Test.all
 
-    render json: @tests
+    render json: tests
   end
 
-  # GET /tests/1
   def show
-    render json: @test
+    render json: test
   end
 
-  # POST /tests
   def create
-    @test = Test.new(test_params)
+    test = Test.new(test_params)
 
-    if @test.save
-      render json: @test, status: :created, location: @test
+    if test.save
+      render json: test#, status: :created, location: test
     else
-      render json: @test.errors, status: :unprocessable_entity
+      render json: test.errors#, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tests/1
-  def update
-    if @test.update(test_params)
-      render json: @test
-    else
-      render json: @test.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if test.update(test_params)
+  #     render json: test
+  #   else
+  #     render json: test.errors#, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /tests/1
-  def destroy
-    @test.destroy
-  end
+  # def destroy
+  #   test.destroy
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_test
-      @test = Test.find(params[:id])
+      test = Test.find_by_id(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def test_params
       params.require(:test).permit(:name, :content)
     end
