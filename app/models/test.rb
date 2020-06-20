f82d52532e2fcb7dfad6ffe681344e792837b194
pharia-le <pharia.le@gmail.com>
2020-06-20 15:18:01 -0700

    Add validations and default_scope to Test.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :content, presence: true
+    validates :content, length: { minimum: 500 }
+    validates :content, length: { maximum: 2000 }
+    default_scope { order(name: :asc) }
 end
