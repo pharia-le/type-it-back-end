e5f2a1cf4871bab5e591a65a3929b537b1da576a
pharia-le <pharia.le@gmail.com>
2020-06-21 15:14:49 -0700

    Remove instance vars., uncomment actions & utilize ScoreSerializer.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,48 +1,56 @@
 class ScoresController < ApplicationController
-  before_action :set_score, only: [:show, :update, :destroy]
+  # before_action :set_score, only: [:show, :update, :destroy]
 
-  # GET /scores
   def index
-    @scores = Score.all
-
-    render json: @scores
+    scores = Score.all
+    options = {
+      include: [:test]
+    }
+    render json: ScoreSerializer.new(scores,options)
   end
 
-  # GET /scores/1
   def show
-    render json: @score
+    score = Score.find(params[:id])
+    options = {
+      include: [:test]
+    }
+    render json: ScoreSerializer.new(score,options)
   end
 
-  # POST /scores
   def create
-    @score = Score.new(score_params)
+    score = Score.new(score_params)
 
-    if @score.save
-      render json: @score, status: :created, location: @score
+    if score.save
+      options = {
+        include: [:test]
+      }
+      render json: ScoreSerializer.new(score,options)#, status: :created, location: score
     else
-      render json: @score.errors, status: :unprocessable_entity
+      render json: score.errors#, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /scores/1
   def update
-    if @score.update(score_params)
-      render json: @score
+    if score.update(score_params)
+      options = {
+        include: [:test]
+      }
+      render json: ScoreSerializer.new(score,options)
     else
-      render json: @score.errors, status: :unprocessable_entity
+      render json: score.errors#, status: :unprocessable_entity
     end
   end
 
-  # DELETE /scores/1
-  def destroy
-    @score.destroy
-  end
+  # # DELETE /scores/1
+  # def destroy
+  #   score.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_score
-      @score = Score.find(params[:id])
-    end
+    # def set_score
+    #   score = Score.find(params[:id])
+    # end
 
     # Only allow a trusted parameter "white list" through.
     def score_params
