996be67f622b809f73e8a7593f2e7ca6d6bc9408
pharia-le <pharia.le@gmail.com>
2020-06-24 20:30:00 -0700

    Update name col to title in default scope.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
     has_many :scores
-    validates :name, presence: true, uniqueness: true
+    validates :title, presence: true, uniqueness: true
     validates :content, presence: true
     validates :content, length: { minimum: 500 }
     validates :content, length: { maximum: 2000 }
-    default_scope { order(name: :asc) }
+    default_scope { order(title: :asc) }
 end
