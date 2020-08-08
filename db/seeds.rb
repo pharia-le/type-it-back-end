d987ee6b20596e8edd00e2512d007946b1c6c968
pharia-le <pharia.le@gmail.com>
2020-08-07 17:07:56 -0700

    Update seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Test.create([{
         title: "Housekeeping",
@@ -31,11 +26,3 @@ Test.create([{
         likes: 0
     }
 ])
-
-# 20.times do
-#     Score.create(
-#         wpm: rand(30..80),
-#         test_id: Test.all.map {|t| t.id}.sample,
-#         accuracy: 100
-#     )
-# end
