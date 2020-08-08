133cea8cd3be8d1b1dab3306f4e59a399c340fb2
pharia-le <pharia.le@gmail.com>
2020-08-07 17:04:05 -0700

    Update methods to utilize active model serializer.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,20 +1,20 @@
 class TestsController < ApplicationController
   def index
-    @tests = Test.all
-    render json: @tests.as_json(include: {scores: {only: [:id,:cpm,:wpm,:accuracy,:errors_count]}})
+    tests = Test.all
+    render json: tests
   end
 
   def show
-    @test = Test.find_by_id(params[:id])
-    render json: @test.as_json(include: {scores: {only: [:id,:cpm,:wpm,:accuracy,:errors_count]}})
+    test = Test.find_by_id(params[:id])
+    render json: test
   end
 
   def update
-    @test = Test.find_by_id(params[:id])
-    if @test.update(test_params)
-      render json: @test.as_json(include: {scores: {only: [:id,:cpm,:wpm,:accuracy,:errors_count]}})
+    test = Test.find_by_id(params[:id])
+    if test.update(test_params)
+      render json: test
     else
-      render json: @test.errors
+      render json: test.errors
     end
   end
 
