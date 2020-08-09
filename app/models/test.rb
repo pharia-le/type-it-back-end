e766f5907ed02133834752f273adf49c695460f3
pharia-le <pharia.le@gmail.com>
2020-08-08 23:06:13 -0700

    Remove unused validations.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,4 @@
 class Test < ApplicationRecord
     has_many :scores
-    validates :title, presence: true, uniqueness: true
-    validates :content, presence: true
-    validates :content, length: { minimum: 500 }
-    validates :content, length: { maximum: 2000 }
     default_scope { order(likes: :desc) }
 end
